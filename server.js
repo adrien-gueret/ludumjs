671ad32d3642c97b7d55df595cff30432e617aba
Adrien Guéret <adrien.grt@gmail.com>
2018-05-01 19:21:51 +0200

    chore(*): keep only one tsconfig file for client and server

--- server.js
+++
@@ -1,7 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-
-const LudumjsServer = require('./dist/server/server/index.js');
-
-for (let x in LudumjsServer) {
-    exports[x] = LudumjsServer[x];
-}
