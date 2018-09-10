2c18c1bdbe15ca199d15e314f5bad5f9eb65d027
Adrien Guéret <adrien.grt@gmail.com>
2018-09-10 21:48:55 +0200

    fix(Phase,server): apply withSocketListeners mixin

--- src/server/lib/Phase.ts
+++ src/server/lib/Phase.ts
@@ -2,9 +2,12 @@ import * as socketio from 'socket.io';
 
 import PhaseCommon from '../../common/lib/Phase';
 import withSockeListeners from '../../common/lib/withSocketListeners';
+import applyMixins from '../../common/utils/applyMixins';
 
 export default abstract class Phase extends PhaseCommon implements withSockeListeners {
     // withSocketListeners
     attachSocketEvent: (socket: socketio.Socket) => void;
     removeSocketEvent: (socket: socketio.Socket) => void;
 }
+
+applyMixins(Phase, [withSockeListeners]);
