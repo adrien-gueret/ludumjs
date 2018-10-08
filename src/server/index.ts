8d21898124bf36504894b5d941ce9a82b936025c
Adrien Guéret <adrien.grt@gmail.com>
2018-10-08 21:22:26 +0200

    feat(server): expose Player class

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 export { default as Game } from './lib/Game';
 export { default as GameFactory } from './lib/GameFactory';
 export { default as Phase } from './lib/Phase';
+export { default as Player } from './lib/Player';
 export * from './errors';
